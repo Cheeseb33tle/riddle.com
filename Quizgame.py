82297543a0600cfef307fb19a99b3ddc34bb2482
Cheeseb33tle <101304848+Cheeseb33tle@users.noreply.github.com>
2022-03-10 13:09:33 +1030

    Add files via upload

---
+++ Quizgame.py
@@ -0,0 +1,29 @@
+print ("Welcome Player")
+
+playing = input("do you like riddles? y/n?")
+
+if playing == ('y'):
+    ("great! i have some for you. If you get them all right ill give you a reward. all the anwsers are 1 word and have no capitals. goodluck <3" )
+else: 
+    print ("oh ok, have a good day :)")
+    quit()
+
+print ("okay, lets play :)")
+
+answer = input ("The more you take, the more you leave behind. What am I?")
+if answer == "footsteps":
+    print ("Correct!")
+
+
+
+answer = input ("I am not alive, but I grow; I don't have lungs, but I need air; I don't have a mouth, but water kills me. What am I?")
+if answer == "fire":
+    print ("Correct!")
+
+
+answer = input ("Always in you, Sometimes on you; If I surround you, I can kill you. What am I?")
+if answer == "water":
+    print ("WOW! your really good at riddles. here is your prize")
+    print ("https://www.funnymonkeysite.com/")
+
+  
